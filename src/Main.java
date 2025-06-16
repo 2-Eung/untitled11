553764dcf7ef185683edfaffb7f365985700aa21
2-Eung <eung4256@gmail.com>
2025-06-16 09:15:20 +0900

    static

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
+    static int count = 0;
+
+    static void printHello() {
+        System.out.println("Hello!");
+    }
+
     public static void main(String[] args) {
     }
 }
