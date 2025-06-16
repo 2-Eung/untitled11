1d4f03e2bb880938e997e3408daeba29e328230f
2-Eung <eung4256@gmail.com>
2025-06-16 13:16:28 +0900

    Inner class 3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 class Outer {
-    String outerMessage = "Hello from Outer";
-
-    class Inner {                                   // private 를 넣으면 안된다 왜???
-        void showMessage() {
-            System.out.println(outerMessage);
+    private class HiddenInner {                         // 외부에 숨기기위한것
+        void secret() {
+            System.out.println("private!");
         }
+    }
+
+    public void reveal() {
+        HiddenInner hi = new HiddenInner();
+        hi.secret();
     }
 }
 
@@ -12,8 +15,5 @@ public class Main {
     public static void main(String[] args) {
         Outer outer = new Outer();
 
-        Outer.Inner inner = outer.new Inner();      // inner 인스턴스는 반드시 outer 인스턴스를 통해 생성해야 한다????
-
-        inner.showMessage();
     }
 }
