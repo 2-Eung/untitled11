3877e173a2643c115799e7c30eecc16b23c4c49b
2-Eung <eung4256@gmail.com>
2025-06-16 14:48:22 +0900

    Anonymous Class

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,16 @@
-class Outer {
-    static class StaticInner {
-        void hello() {
-            System.out.println("Hi!");
-        }
-    }
+interface Greeting {
+    void greet();
 }
 
-
-
-
-
 public class Main {
-
     public static void main(String[] args) {
-//      Outer outer = new Outer();                              // inner 에 static 이 없으면 이렇게
-//      Outer.StaticInner inner = outer.new StaticInner();      // inner 에 static 이 없으면 이렇게
+        Greeting greeting = new Greeting() {                // Greeting 이게 anonymousClass
+            @Override                                       // 원래라면 위에 클래스에 상속에 오버라이딩까지 해야하지만
+            public void greet() {                           // 한번쓰고 말거여서 이렇게 한다.
+                System.out.println("Hello from the anonymous class!");
+            }
+        };
 
-        Outer.StaticInner inner = new Outer.StaticInner();      // 있다면 이렇게
-        inner.hello();
+        greeting.greet();
     }
 }
