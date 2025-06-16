8c89f3f59bd73508069c9cc26c180d0d87ed0548
2-Eung <eung4256@gmail.com>
2025-06-16 14:32:23 +0900

    Inner class 5 Inner without Outer with static (static Nested class)

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,22 @@
-public class Main {
-    private String data = "Outer data";
-
-    class Inner {
-        void printData() {
-            System.out.println(data);
+class Outer {
+    static class StaticInner {
+        void hello() {
+            System.out.println("Hi!");
         }
     }
+}
 
-    public Inner createInner() {                        // 반환타입이 Inner
-        return new Inner();                             // new ....
-    }
+
+
+
+
+public class Main {
 
     public static void main(String[] args) {
-        Main outer = new Main();
+//      Outer outer = new Outer();                              // inner 에 static 이 없으면 이렇게
+//      Outer.StaticInner inner = outer.new StaticInner();      // inner 에 static 이 없으면 이렇게
 
-        //Outer.this
-        Main.Inner inner = outer.createInner();
-
-        inner.printData();
-
-        outer = null;                                   // outer 메모리를 날려도
-        inner.printData();                              // 실행 됨
-
-        inner = null;
-//      inner.printData();                              // inner 메모리 날리면 실행 안됨
+        Outer.StaticInner inner = new Outer.StaticInner();      // 있다면 이렇게
+        inner.hello();
     }
 }
