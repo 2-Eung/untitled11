1e0eb481c84ee254bc3a9ec0a46a659f7afa2913
2-Eung <eung4256@gmail.com>
2025-06-16 10:41:05 +0900

    Inner class

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,21 @@
-class Person{
-    int a = 1;                      // 인스턴스 변수
-    static int b = 2;               // static 변수
 
+public class Main {             // Outer class
+    private String message = "Hello from the outer class";
 
-
-    public static void greet() {
-        System.out.println("sadfasd");
-
-//      System.out.println(a);      // static 함수 에서는 인스턴스 변수에 접근 불가 왜??
-                                    // static 변수 는 클래스를 로드할때 호출되지만 인스턴스 변수는 인스턴스가 생성될때 호출되어
-                                    // 이 시점에서는 아직 인스턴스 변수가 존재하지 않는다!
-        System.out.println(b);
+    class InnerMain {           // Inner class
+        void displayMessage() {
+            System.out.println("Inner says : " + message); // Outer 와 같은 클래스처럼 여겨져 private 도 접근 가능
+        }
     }
 
-    public void sayHello() {
-        System.out.println("Hello");
-
-        System.out.println(a);      // 인스턴스 함수니까 접근 가능
-        System.out.println(b);
+    public void callInner() {                   // main에 안쓰고 왜 callInner 를 따로 만든거거지?
+        InnerMain inner = new InnerMain();      // static 을 쓴다면 이거 없이도 가능할까???
+        inner.displayMessage();
     }
-}
-public class Main {
+
+
     public static void main(String[] args) {
-        Person.greet();
-
-        Person p = new Person();    // static 만 있을경우 노란줄로 경고메세지의 의미는???
-        p.greet();                  // 어짜피 객체 생성해도 같은결과?? 를 만들어내니까 의미가없다라는뜻????
-        p.sayHello();
-
+        Main outer = new Main();                // 왜 Main 이 생략안되지???
+        outer.callInner();
     }
 }
